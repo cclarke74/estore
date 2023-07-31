4de2b6de7375afcad8e3fa4e7cb6ad2ea528dbec
cclarke74 <clairc2@gmail.com>
2023-07-31 16:06:08 -0700

    added router links

---
+++ Documents/Angular/estore/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {ProfileComponent} from './profile/profile.component';
+import {ProductsComponent} from './products/products.component';
+import { HomeComponent } from './home/home.component';
+
+
+
+const routes: Routes = [
+  {path:'home', component: HomeComponent },
+  {path:'products', component: ProductsComponent },
+  {path:'profile', component: ProfileComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Documents/Angular/estore/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileComponent,
+    ProductsComponent,
+    HomeComponent
+     
+  ],
+
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
