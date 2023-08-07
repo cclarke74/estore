18f67c20d44a15821355316503fe2fed61f526f5
cclarke74 <clairc2@gmail.com>
2023-08-06 20:59:54 -0700

    add product services

--- Documents/Angular/estore/src/app/app-routing.module.ts
+++ Documents/Angular/estore/src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import {ProfileComponent} from './profile/profile.component';
 import {ProductsComponent} from './products/products.component';
 import { HomeComponent } from './home/home.component';
+import {PostsComponent} from './posts/posts.component';
 
 
 
 const routes: Routes = [
   {path:'home', component: HomeComponent },
   {path:'products', component: ProductsComponent },
-  {path:'profile', component: ProfileComponent }
+  {path:'profile', component: ProfileComponent },
+  {path:'posts', component: PostsComponent }
+
 ];
 
 @NgModule({
